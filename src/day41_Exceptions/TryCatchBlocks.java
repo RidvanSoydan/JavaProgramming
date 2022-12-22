3ffab822bd60c3358ae8e58e0c71a41badf77542
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2022-12-22 22:53:01 +0300

    Exceptions

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,10 +1,27 @@
 package day41_Exceptions;
 
+import java.sql.SQLOutput;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
 
+        System.out.println("Test Started");
 
+        try {  //try block did not executed
+
+            System.out.println(9/0);    // Not executed
+            System.out.println("Try Block"); // Not executed
+
+        } catch (ArithmeticException e){
+
+            System.out.println("Catch Block");
+            System.out.println("Arithmetic Exception was occurred");
+        }
+
+       // System.out.println(9/0); // java.lang.ArithmeticException: / by zero
+
+        System.out.println("Test Completed");
 
 
 
