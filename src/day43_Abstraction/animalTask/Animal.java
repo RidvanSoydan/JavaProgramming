8130115b2fd2d304045d57544f1f010d24c166f0
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2023-01-01 20:40:05 +0300

    Abstraction

---
+++ src/day43_Abstraction/animalTask/Animal.java
@@ -0,0 +1,29 @@
+package day43_Abstraction.animalTask;
+
+public class Animal {
+
+
+    private String name, breed;
+    private int age;
+    private char gender;
+    private String size, color;
+
+
+}
+/*
+
+Animal task:
+
+		abstract Animal
+
+			eat();
+
+		Dog
+
+		Cat
+
+		Tiger
+
+		Eagle
+
+ */
