37f12c35a5d705c0f39e6d10d155fd87aec554e1
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2023-01-01 13:55:57 +0300

    Exceptions

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,24 +1,25 @@
 package day42_Exceptions;
 
-public class CustomExceptions {
-
-
     class FadyException extends RuntimeException{ // custom unchecked exception
 
-        public FadyException(){
-            super("Time for a short break");
-        }
-
-        public FadyException(String message){
-            super(message);
-        }
-
+    public FadyException(){
+        super("Time for a short break");
     }
 
-
-    static class NoBreakException extends Exception{ // custom checked exception
-
+    public FadyException(String message){
+        super(message);
     }
+
+}
+
+
+    class NoBreakException extends Exception{ // custom checked exception
+
+}
+
+
+    public class CustomExceptions {
+
 
     public static void main(String[] args)  {
 
@@ -33,4 +34,5 @@ public class CustomExceptions {
 
 
     }
+
 }
