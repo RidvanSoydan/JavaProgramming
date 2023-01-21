5522603905bd937ee427b59b714bbaddbec01056
Rıdvan Soydan <ridvansoydan0711@gmail.com>
2023-01-21 17:42:29 +0300

    Collections

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,6 @@
-import day39_Encapsulation_Inheritance_Recap.cydeoTask.Employee;
-
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.*;
-
 public class Test {
 
 
-    public static void main(String[] args) {
 
-
-
-
-    }
 
 }
-
